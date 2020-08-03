0d677b8d6389adec18d4447d61263aa239c480a5
Hexiang Zhang <sam10.zhang@gmail.com>
2020-08-03 16:12:49 -0500

    draw paddle in game.js

---
+++ game.js
@@ -0,0 +1,34 @@
+// SELECT CANVAS ELEMENTS
+const cvs = document.getElementById("breakout");
+const ctx = cvs.getContext("2d");
+
+// ADD BORDER TO CANVAS 
+cvs.style.border = "3px solid #0ff";
+
+// SET PADDLE THICKNESS
+ctx.lineWidth = 3;
+
+// SET GAME VARIABLES AND CONSTANTS
+const PADDLE_WIDTH = 100;
+const PADDLE_MARGIN_BOTTOM = 50;
+const PADDLE_HEIGHT = 20;
+
+// CREATE PADDLE
+const paddle = {
+    x: cvs.width / 2 - PADDLE_WIDTH / 2,
+    y: cvs.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+    width: PADDLE_WIDTH,
+    height: PADDLE_HEIGHT,
+    dx: 5
+}
+
+// DRAW PADDLE
+function drawPaddle() {
+    ctx.fillStyle = "#2e3548";
+    ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+
+    ctx.strokeStyle = "#ffcd05";
+    ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
+
+drawPaddle();
