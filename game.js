fe0613d0865027cb4a1a7dfa886ecc1d5665abe8
Hexiang Zhang <sam10.zhang@gmail.com>
2020-08-06 16:25:18 -0500

    move paddle within canvas

--- game.js
+++ game.js
@@ -12,6 +12,8 @@ ctx.lineWidth = 3;
 const PADDLE_WIDTH = 100;
 const PADDLE_MARGIN_BOTTOM = 50;
 const PADDLE_HEIGHT = 20;
+let leftArrow = false;
+let rightArrow = false;
 
 // CREATE PADDLE
 const paddle = {
@@ -31,15 +33,40 @@ function drawPaddle() {
     ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
 }
 
+// CONTROL PADDLE
+document.addEventListener("keydown", function(event) {
+    if(event.keyCode == 37) {
+        leftArrow = true;
+    } else if(event.keyCode == 39) {
+        rightArrow = true;
+    }
+});
+
+// MOVE PADDLE
+function movePaddle() {
+    if(leftArrow && paddle.x > 0) {
+        paddle.x -= paddle.dx;
+    } else if(rightArrow && (paddle.x + paddle.width) < cvs.width) {
+        paddle.x += paddle.dx; 
+    }
+}
+
+document.addEventListener("keyup", function(event) {
+    if(event.keyCode == 37) {
+        leftArrow = false;
+    } else if(event.keyCode == 39) {
+        rightArrow = false;
+    }
+});
+
 // DRAW FUNCTION
 function draw() {
     drawPaddle();
-
 }
 
 // UPDATE GAME FUNCTION
 function update() {
-
+    movePaddle();
 }
 
 // GAME LOOP 
