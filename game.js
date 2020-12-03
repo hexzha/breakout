c2752788353d8d87ab16af58be6b523efbbc386e
Hexiang Zhang <sam10.zhang@gmail.com>
2020-12-03 20:19:19 +0100

    update

--- game.js
+++ game.js
@@ -152,7 +152,9 @@ function createBricks() {
         bricks[r] = [];
         for( let c = 0; c < brick.column; c++) {
             brick[r][c] = {
-                
+                x : c * (brick.offSetLeft + brick.width ) + brick.offSetLeft,
+                y : r * (brick.offSetTop + brick.height ) + brick.offSetTop + brick.marginTop, 
+                status : true
             }
         }
     }
